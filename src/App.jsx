39bc7e1867cec531b9c83c3016cd907d05f68f33
Israel David Dahan <israeldaviddahan@gmail.com>
2024-07-18 10:57:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/')
+      const data = await response.json()
+      console.log(data)
+      setData(data)
+    }
+    fetchData()
+  })
+
+  return (
+    <>
+        <h1> This is get from Backend</h1>
+        <div className="card">
+        <p>{data.msg}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
